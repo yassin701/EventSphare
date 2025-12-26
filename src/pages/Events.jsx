4037492e95acd62ff3a3fede90a1a7a45079d665
Yassine <yassinehamdoune55@gmail.com>
2025-12-26 16:08:24 +0100

    add searsh bar and category

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -3,49 +3,86 @@ import axios from "axios";
 import EventCard from "../Components/EventCard";
 
 export default function Events() {
-  const [events, setEvents] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [events, setEvents] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState("");
+    const [selectedCategory, setSelectedCategory] = useState("All");
+    const categories = ["All", "Music", "Art", "Sport", "Theatre", "Other"];
 
-  useEffect(() => {
-    fetchEvents();
-  }, []);
 
-  const fetchEvents = async () => {
-    try {
-      const res = await axios.get(
-        "https://694d36b2ad0f8c8e6e200cec.mockapi.io/api/v1/event"
-      );
-      setEvents(res.data);
-    } catch (error) {
-      console.error("Error fetching events", error);
-    } finally {
-      setLoading(false);
+    const filteredProducts = events
+        .filter(product =>
+            selectedCategory === "All" ? true : product.category === selectedCategory
+        )
+        .filter(event =>
+            event.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+        );
+
+    useEffect(() => {
+        fetchEvents();
+    }, []);
+
+    const fetchEvents = async () => {
+        try {
+            const res = await axios.get(
+                "https://694d36b2ad0f8c8e6e200cec.mockapi.io/api/v1/event"
+            );
+            setEvents(res.data);
+        } catch (error) {
+            console.error("Error fetching events", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center min-h-screen">
+                Loading events...
+            </div>
+        );
     }
-  };
 
-  if (loading) {
     return (
-      <div className="flex justify-center items-center min-h-screen">
-        Loading events...
-      </div>
+        <div className="bg-green-50 min-h-screen p-6">
+            <h1 className="text-3xl font-bold text-center text-green-600 mb-8 mt-5">
+                Events
+            </h1>
+
+            {/* Category + Search */}
+            <div className='flex flex-col md:flex-row justify-between items-center gap-4 mb-8'>
+                {/* Category dropdown */}
+                <select
+                    value={selectedCategory}
+                    onChange={(e) => setSelectedCategory(e.target.value)}
+                    className='border rounded-lg px-4 py-2 w-full md:w-60'
+                >
+                    {categories.map(cat => (
+                        <option key={cat} value={cat}>
+                            {cat}
+                        </option>
+                    ))}
+                </select>
+
+                {/* Search input */}
+                <input
+                    type="text"
+                    placeholder='Search product...'
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className='border rounded-lg px-4 py-2 w-full md:w-72 focus:outline-none'
+                />
+            </div>
+
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                {filteredProducts.map((event) => (
+                    <EventCard
+                        key={event.id}
+                        event={event}
+                        isAdmin={false}   // 👈 user mode
+                    />
+                ))}
+            </div>
+        </div>
     );
-  }
-
-  return (
-    <div className="bg-green-50 min-h-screen p-6">
-      <h1 className="text-3xl font-bold text-center text-green-600 mb-8 mt-5">
-       Events
-      </h1>
-
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {events.map((event) => (
-          <EventCard
-            key={event.id}
-            event={event}
-            isAdmin={false}   // 👈 user mode
-          />
-        ))}
-      </div>
-    </div>
-  );
 }
