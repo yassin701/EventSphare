f4ab93f1981fc6be1b293dd7699586aa238073cb
Yassine <yassinehamdoune55@gmail.com>
2025-12-28 00:24:59 +0100

    create redux for safty cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import cartReducer from "./cartSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
 
-// export const store = configureStore({
-//   reducer: {
-//     cart: cartReducer,
-//   },
-// });
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 
 
